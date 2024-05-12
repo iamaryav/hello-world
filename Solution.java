3730bda27e37911f6a4865f47a0d1d652af8b185
Priya Aryav <priya.aryav@gmail.com>
2024-05-13 00:40:51 +0530

    practice

---
+++ Solution.java
@@ -0,0 +1,20 @@
+public class CustomSort{
+
+	private Map<String, Integer> sort(Map<String, Integer> input){
+		//TODO
+
+        Map<String, Integer> sortedInput;
+
+        
+	}
+
+	public static void main(String[] arsg){
+		Map<String, Integer> input = new HashMap<>();
+		input.put("Green", 3);
+		input.put("Red", 8);
+		input.put("Blue", 5);
+
+		Map<String, Integer> output = new CustomSort().sort(input);
+
+	}
+}
