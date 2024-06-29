40672624b64d161e72be6070e35eef462478b3d3
Priya Aryav <priya.aryav@gmail.com>
2024-06-29 23:11:06 +0530

    practice

---
+++ dsa/Graph.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Graph{
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public void printGraph(){
+        System.out.println(adjList);
+    }
+
+    public boolean addVertex(String vertex){
+        if(adjList.get(vertex) == null){
+            adjList.put(vertex, new ArrayList<>());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addEdge(String vertex1, String vertex2){
+        if(adjList.get(vertex1) != null && adjList.get(vertex2) != null){
+            adjList.get(vertex1).add(vertex2);
+            adjList.get(vertex2).add(vertex1);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeEdge(String vertex1, String vertex2){
+        if(adjList.get(vertex1) != null && adjList.get(vertex2) != null){
+            adjList.get(vertex1).remove(vertex2);
+            adjList.get(vertex2).remove(vertex1);
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean removeVertex(String vertex){
+        if(adjList.get(vertex) == null){
+            return false;
+        }
+        for(String otherVertex : adjList.get(vertex)){
+            adjList.get(otherVertex).remove(vertex);
+        }
+        adjList.remove(vertex);
+        return false;
+    }
+
+    public static void main(String[] args){
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.printGraph();
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "D");
+        graph.addEdge("C", "D");
+        graph.addEdge("A", "D");
+        graph.printGraph();
+        // graph.removeEdge("A", "B");
+        graph.removeVertex("D");
+        graph.printGraph();
+    }
+}
